758cab088f19f8455ed15c386ea8119c8ed82409
Enrique0309 <80069857+Enrique0309@users.noreply.github.com>
2021-03-26 19:25:27 -0700

    Add files via upload
    arreglos

---
+++ arreglosp1.js
@@ -0,0 +1,67 @@
+/*var arregloNom = [ "juan","pedro","kike","pablo" ]
+
+console.log ( arregloNom [ 2 ] );
+arregloNom [ 2 ] = "enrique";
+
+console.log( arregloNom [ 2 ] );
+
+delete arregloNom[2];
+console.log (arregloNom);
+
+console.log (arregloNom.length);
+
+//"push" agrega un elemento al arreglo
+
+arregloNom.push ( "luis" );
+console.log (arregloNom );
+
+//"pop" elimina uyn elemento del arreglo
+
+arregloNom.pop ();
+console.log (arregloNom);
+
+//metodos de arreglos
+
+arregloNom.splice ( 0, 0, "pancho" );
+console.log(arregloNom);
+
+arregloNom.splice( 1,4 );
+console.log( arregloNom );
+
+//realiza un programa que lea por teclado 5 notas/calif y muestre el promedio de estas
+
+const  prompt  =  require ( 'prompt-sync' ) ( ) ;
+
+var calif = [];
+
+calif[0] = prompt("dame la primera calificacion: ");
+//console.log (calificaciones [0]);
+
+calif[1] = prompt("dame la segunda calificacion: ");
+calif[2] = prompt("dame la tercera calificacion: ");
+calif[3] = prompt("dame la cuarta calificacion: ");
+calif[4] = prompt("dame la quinta calificacion: ");
+
+console.log (calif );
+
+var resultado = parseInt (calif[0]) + parseInt(calif[1]) + parseInt(calif[2])
+                 + parseInt(calif[3]) + parseInt(calif[4]);
+
+resultado /= 5;
+
+console.log( resultado );
+*/
+//concatenar(juntar) 2 arreglos
+
+var concatena1 = ["hola", "como","estas" ];
+var concatena2 = ["adios", "buena", "suerte" ];
+
+console.log (concatena1.concat (concatena2));
+
+
+
+
+
+
+
+
